d8b76b7a962d4b38d4c019dc3b2e2f65a2884b99
andresbueno043 <ajbueno@iegabo.edu.co>
2023-05-20 07:05:37 -0500

    Fetch function from catfact api

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,27 @@
+import { useEffect, useState } from 'react'
+
 function App() {
-  return (
-    <>
-      <div className="flex justify-center items-center rounded-xl bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 h-36 w-[60%] mx-auto mt-8">
-        <div className="flex justify-center items-center w-[98%] h-[90%] rounded-md bg-gray-800">
-          <h1 className="text-white text-xl">Buenas noches</h1>
+    const [fact, setFact] = useState<string>('')
+
+    useEffect(() => {
+        fetch('https://catfact.ninja/fact')
+            .then((res) => res.json())
+            .then((data) => {
+                const catFact = data.fact
+                setFact(catFact)
+            })
+            .catch((error) => {
+                console.error('Error al obtener el hecho sobre gatos:', error)
+            })
+    }, [])
+
+    return (
+        <div className="flex justify-center items-center rounded-xl bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 h-36 w-[60%] mx-auto mt-8">
+            <div className="flex justify-center items-center w-[98%] h-[90%] rounded-md bg-gray-800 px-10 py-6">
+                <h1 className="text-white text-xl">{fact}</h1>
+            </div>
         </div>
-      </div>
-    </>
-  );
+    )
 }
 
-export default App;
+export default App
