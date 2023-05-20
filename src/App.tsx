4fa37e58bf91bbad94150a63f14fed96dcb3394f
andresbueno043 <ajbueno@iegabo.edu.co>
2023-05-19 19:07:19 -0500

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <>
+      <div className="flex justify-center items-center rounded-xl bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 h-36 w-[60%] mx-auto mt-8">
+        <div className="flex justify-center items-center w-[98%] h-[90%] rounded-md bg-gray-800">
+          <h1 className="text-white text-xl">Buenas noches</h1>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
