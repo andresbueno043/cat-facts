6d35c5f4a29079f7afba4946fba20b75f204548d
andresbueno043 <ajbueno@iegabo.edu.co>
2023-05-22 08:47:10 -0500

    glowing button component created

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,13 @@
 import FactBox from './components/FactBox'
 import GifList from './components/GifList'
+import GlowingButton from './components/GlowingButton'
 
 function App() {
     return (
         <>
             <FactBox />
             <GifList />
-            <div className="flex items-center justify-center">
-                <a href="#">
-                    <span>Button</span>
-                    <i></i>
-                </a>
-            </div>
+            <GlowingButton />
         </>
     )
 }
---
+++ src/components/GlowingButton.tsx
@@ -0,0 +1,12 @@
+function GlowingButton() {
+    return (
+        <div className="flex items-center justify-center">
+            <a href="#">
+                <span>Button</span>
+                <i></i>
+            </a>
+        </div>
+    )
+}
+
+export default GlowingButton
