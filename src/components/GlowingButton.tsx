38b353354d288545d35af24290799033a773a65e
andresbueno043 <ajbueno@iegabo.edu.co>
2023-05-23 10:10:41 -0500

    Glowing button tailwind css

--- src/components/GlowingButton.tsx
+++ src/components/GlowingButton.tsx
@@ -1,9 +1,12 @@
 function GlowingButton() {
     return (
         <div className="flex items-center justify-center">
-            <a href="#">
-                <span>Button</span>
-                <i></i>
+            <a
+                href="#"
+                className="relative bg-white text-white text-uppercase text-lg font-semibold tracking-wider py-2 px-6 transition-all duration-500 ease-in-out transform hover:letter-spacing-xl hover:bg-blue-500 hover:text-blue-500 hover:shadow-lg"
+            >
+                <span className="relative z-10">Button</span>
+                <i className="absolute inset-0"></i>
             </a>
         </div>
     )
