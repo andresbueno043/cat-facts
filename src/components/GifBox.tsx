65888c1354f2db48509bf347a2f1018ba92e2fbf
andresbueno043 <ajbueno@iegabo.edu.co>
2023-05-23 10:19:02 -0500

    GifBox styling changed

--- src/components/GifBox.tsx
+++ src/components/GifBox.tsx
@@ -6,13 +6,14 @@ type Props = {
 
 function GifBox({ gifUrl }: Props) {
     return (
-        <div className="flex items-center rounded-md justify-center bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 w-[30%] h-[80%]">
-            <div className="rounded-md object-cover w-[95%] h-[94%] bg-gray-800">
+        <div className="flex items-center rounded-md justify-center w-[30%] h-[80%] relative bg-white text-white text-uppercase text-lg font-semibold tracking-wider transition-all duration-500 ease-in-out transform hover:letter-spacing-xl hover:bg-blue-500 hover:text-blue-500 hover:shadow-lg p-1 y-1">
+            <div className="rounded-md object-cover w-full h-full bg-gray-800">
                 <img
                     className="w-full h-full border-black border-[1px]"
                     src={gifUrl}
                     alt=""
                 />
+                <i></i>
             </div>
         </div>
     )
